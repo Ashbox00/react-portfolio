ce73d30810b24bcd8754b44883a6e63a12f1bb8b
Gage <gageashbaughg@gmail.com>
2024-05-17 23:41:34 -0700

    added section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import projects from "../../data/projects.json";
 import { getImageUrl } from "../../utils";
 import { ProjectCard } from "./ProjectCard";
+import styles from "./Projects.module.css";
 export const Projects = () => {
-    return <section>
-        <h2>Projects</h2>
-        <div>
+    return <section className={styles.container}>
+        <h2 className={styles.title}><u>Projects</u></h2>
+        <div className={styles.project}>
             {
                 projects.map((project,id) => {
-                    return(
-                        <ProjectCard key ={id}/>
+                    return (
+                        <div key={id} className={styles.projectCard}>
+                            <img src ={getImageUrl(project.imageSrc)}></img>
+                            <div className={styles.description}>
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                            </div>
+                            <ul>
+                                {
+                                    project.skills.map((skill, id)=> {
+                                        <li key={id}>{skill}</li>
+                                    })
+                                }
+                            </ul>
+                            <div>
+                                <a href={project.source}>Source</a>
+                            </div>
+                    </div>
                     )
                 }
             )}
